e717ed7ce8159653de20651a797d9d81d925381b
Sesethu <seshmomoza20@gmail.com>
2022-02-21 16:35:04 +0200

    changed to a new branch

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+var msgBtn = document.querySelector(".msgBtn");
+let randomNumber = Math.ceil((Math.random() * 100))
+
 function guess(){
 
-    
+    if (guessNumber < randomNumber) {
+        msgBtn.innerHTML= "Your guess is too low";
+    }else if(guessNumber > randomNumber){
+        msgBtn.innerHTML= "Your guess is too high";
+    }else
+    msgBtn.innerHTML = 'Correct! the secret number is ${}'
 }
