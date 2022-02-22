f595f16b64cb19009906d3f91f9929fe008e48ba
Sesethu <seshmomoza20@gmail.com>
2022-02-22 15:53:48 +0200

    added a display element

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-var msgBtn = document.querySelector(".msgBtn");
+var submitBtn = document.querySelector(".submitBtn");
+var select = document.querySelector(".select");
+var resultElem = document.querySelector(".result")
+var guessedNumber;
+
 let randomNumber = Math.ceil((Math.random() * 100))
 
-function guess(){
+function guess()
+{
+    if (document.querySelector(".select").value != null) 
+    {
+        guessedNumber= document.querySelector(".select").value;
+    }
+    document.querySelector(".result").innerHTML = guessedNumber;
+    console.log(guessedNumber);
 
-    if (guessNumber < randomNumber) {
-        msgBtn.innerHTML= "Your guess is too low";
-    }else if(guessNumber > randomNumber){
-        msgBtn.innerHTML= "Your guess is too high";
-    }else
-    msgBtn.innerHTML = 'Correct! the secret number is ${}'
+    if (guessedNumber < randomNumber) 
+    {
+        //return "your guess is too low";
+        resultElem.innerHTML= "Your guess is too low";
+    }
+    else if(guessedNumber > randomNumber){
+        resultElem.innerHTML ="Your guess is too high";
+    
+    }
+    else
+    return `Correct, the secret number is ${randomNumber}`;
 }
+
+submitBtn.addEventListener("click",guess);
+
