8ec9ae6d2120ebf805172aba4dc09160400de2eb
Sesethu <seshmomoza20@gmail.com>
2022-02-23 16:13:35 +0200

    fixed an error

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var submitBtn = document.querySelector(".submitBtn");
 var select = document.querySelector(".select");
-var resultElem = document.querySelector(".result")
+var resultElem = document.querySelector(".result");
 var guessedNumber;
 
-let randomNumber = Math.ceil((Math.random() * 100))
+let randomNumber = Math.ceil((Math.random() * 100));
 
 function guess()
 {
-    
-
     if (document.querySelector(".select").value != null) 
     {
         guessedNumber= document.querySelector(".select").value;
@@ -20,18 +18,21 @@ function guess()
     {
         resultElem.innerHTML= "Your guess is too low";
     }
-    else if(guessedNumber > randomNumber){
+    else if(guessedNumber > randomNumber)
+    {
         resultElem.innerHTML ="Your guess is too high";
-    
     }
-    else
-    resultElem.innerHTML = `Correct, the secret number is ${randomNumber}`;
-
+    else if(guessedNumber == randomNumber){
+        resultElem.innerHTML = `Correct, the secret number is ${randomNumber}`;
+    
     setTimeout(function(){
-		resultElem.innerHTML = "";
-	}, 2000);
-  
+		resultElem.innerHTML = "New game started!";
+        //randomNumber = Math.ceil((Math.random() * 100));
+       }, 3000);
+    }
+    setTimeout(function(){
+        resultElem.innerHTML = "";
+    }, 2000);
 }
-
 submitBtn.addEventListener("click",guess);
 
